f5e15b7af237dace116e556203954f0e14236275
Jun Rao <jr2339@nau.edu>
2020-05-09 16:14:22 -0700

    Upload Project9

--- 2020S/Project9/src/p9_package/OpCodeHeapClass.java
+++ 2020S/Project9/src/p9_package/OpCodeHeapClass.java
@@ -1,128 +1,133 @@
 package p9_package;
 
-public class OpCodeHeapClass {
-
-    /**
-     * Initial array capacity
+public class OpCodeHeapClass 
+{
+	/**
+     * Management data for array
      */
-    public final int DEFAULT_ARRAY_CAPACITY=10;
-
-    /**
-     * Array for heap
-     */
-    private OpCodeClass[] heapArray;
-
+    private int arrayCapacity;
     /**
      * Management data for array
      */
     private int arraySize;
-
     /**
-     * Management data for array
+     * Initial array capacity
      */
-    private int arrayCapacity;
-
+    public final int DEFAULT_ARRAY_CAPACITY = 10;
     /**
      * Display flag can be set to observe bubble up and trickle down operations
      */
     private boolean displayFlag;
-
     /**
-     * Default constructor sets up array management conditions and default display flag setting
+     * Array for heap
+     */
+    private OpCodeClass[] heapArray;
+    
+    /**
+     * Default constructor sets up array management 
+     * conditions and default display flag setting
      */
     public OpCodeHeapClass()
     {
-        arrayCapacity = DEFAULT_ARRAY_CAPACITY;
-        arraySize = 0;
-        heapArray = new OpCodeClass[arrayCapacity];
+        this.arrayCapacity = DEFAULT_ARRAY_CAPACITY;
+        this.arraySize = 0;
         displayFlag = false;
+        heapArray = new OpCodeClass[this.arrayCapacity];
     }
-
     /**
-     * Copy constructor copies array and array management conditions and default display flag setting
-     * @param copied - OpCodeHeapClass object to be copied
+     * Copy constructor copies array and array management conditions
+     * and default display flag setting
+     * @param copied - GenericHeapClass object to be copied
      */
     public OpCodeHeapClass(OpCodeHeapClass copied)
     {
-        arraySize = copied.arraySize;
-        arrayCapacity = copied.arrayCapacity;
-        displayFlag = copied.displayFlag;
-        int index = 0;
-        for(index = 0 ; index < copied.arraySize ; index ++)
+        int index;
+        
+        this.arrayCapacity = copied.arrayCapacity;
+        this.arraySize = copied.arraySize;
+        this.displayFlag = copied.displayFlag;
+        this.heapArray = new OpCodeClass[this.arrayCapacity];
+        
+        for(index = 0; index < this.arraySize; index++)
         {
-            heapArray[index] = copied.heapArray[index];
+            this.heapArray[index] = copied.heapArray[index];
         }
     }
-
+    
     /**
-     * Accepts OpCodeData item and adds it to heap
-     * Note: uses bubbleUpArrayHeap to resolve unbalanced heap after data addition
+     * Note: uses bubbleUpArrayHeap to resolve
      * Note: must check for resize before attempting to add an item
-     * @param newItem - OpCodeClass data item to be added
+     * unbalanced heap after data addition
+     * @param newItem - generic data item to be added to heap
      */
     public void addItem(OpCodeClass newItem)
     {
         checkForResize();
         heapArray[arraySize] = newItem;
-        if(displayFlag)
-        {
-            System.out.println("Adding new process: " + newItem);
-        }
-        arraySize ++;
-        if(arraySize>1)
-        {
-            bubbleUpArrayHeap(arraySize-1);
-        }
+        bubbleUpArrayHeap(arraySize);
+        arraySize++;
     }
-
+    
     /**
-     * Recursive operation to reset data in the correct order for the min heap after new data addition
-     * @param currentIndex - index of current item being assessed, and moved up as needed
+     * Recursive operation to reset data in the correct 
+     * order for the min heap after new data addition
+     * @param currentIndex - index of current item being 
+     * assessed, and moved up as needed
      */
     private void bubbleUpArrayHeap(int currentIndex)
     {
-        int parentIndex;
-        OpCodeClass localItem;
-        OpCodeClass parentItem;
-        if (currentIndex > 0 && currentIndex < arraySize)
+    	OpCodeClass OpCode1 = (OpCodeClass)heapArray[currentIndex];
+    	OpCodeClass OpCode2 = (OpCodeClass)heapArray[(currentIndex - 1) / 2];
+        int parentIndex = (currentIndex - 1) / 2;
+        
+        if(displayFlag)
         {
-            localItem = heapArray[currentIndex];
-            parentIndex = (currentIndex - 1) / 2;
-            parentItem = heapArray[parentIndex];
-            if (localItem.compareTo(parentItem) < 0)
+            System.out.println("Adding new process: " + heapArray[currentIndex].toString());
+            
+            if(OpCode1.compareTo(OpCode2) < 0)
             {
-                if (displayFlag)
-                {
-                    System.out.println("   - Bubble up: ");
-                    System.out.println("     - Swapping parent: " +
-                            parentItem + "; with child: " + localItem);
-                }
-                heapArray[parentIndex] = localItem;
-                heapArray[currentIndex] = parentItem;
+                System.out.println(" - Bubble Up: ");
+                System.out.println(" - Swapping Parent: " + 
+                		heapArray[parentIndex].toString() +
+                			"; with child: " +heapArray[currentIndex].toString());
+                System.out.println();
+                heapArray[currentIndex] = OpCode2;
+                heapArray[parentIndex] = OpCode1;
                 bubbleUpArrayHeap(parentIndex);
             }
+            System.out.println();
         }
+            if(OpCode1.compareTo(OpCode2) < 0)
+            {
+                heapArray[currentIndex] = OpCode2;
+                heapArray[parentIndex] = OpCode1;
+                bubbleUpArrayHeap(parentIndex);
+            }
     }
-
+    
     /**
-     * Automatic resize operation used prior to any new data addition in the heap
-     * Tests for full heap array, and resizes to twice the current capacity as required
+     * Automatic resize operation used prior to any new data 
+     * addition in the heap
+     * Tests for full heap array, and resizes 
+     * to twice the current capacity as required
      */
     private void checkForResize()
     {
-        int index=0;
-        if (arraySize == arrayCapacity)
+        OpCodeClass[] resizeArr;
+        int index;
+        
+        if(arraySize == arrayCapacity)
         {
             arrayCapacity = arrayCapacity * 2;
-            OpCodeClass[] newHeapArray = new OpCodeClass[arrayCapacity];
-            for (index = 0; index < arraySize; index ++)
+            resizeArr = new OpCodeClass[arrayCapacity];
+            for(index = 0; index < arraySize; index++)
             {
-                newHeapArray[index] = heapArray[index];
+            	resizeArr[index] = heapArray[index];
             }
-            heapArray = newHeapArray;
+            heapArray = resizeArr;
         }
     }
-
+    
     /**
      * Tests for empty heap
      * @return boolean result of test
@@ -131,88 +136,154 @@ public class OpCodeHeapClass {
     {
         return arraySize == 0;
     }
-
+    
     /**
-     * Removes OpCodeClass data item from top of min heap, thus being the operation
-     * with the lowest priority value
-     * Note: Uses trickleDownArrayHeap to resolve unbalanced heap after data removal
-     * @return OpCodeClass item removed
+     * Removes GenericData item from top of min heap, 
+     * thus being the operation with the lowest priority value
+     * Note: Uses trickleDownArrayHeap to resolve unbalanced 
+     * heap after data removal
+     * @return - GenericData item removed
      */
     public OpCodeClass removeItem()
     {
-        if (!isEmpty()){
-            OpCodeClass removeItem = new OpCodeClass();
-            removeItem = heapArray[0];
-            heapArray[0] = heapArray[arraySize - 1];
-            heapArray[arraySize-1] = null;
-            arraySize -- ;
-            if (displayFlag)
-            {
-                System.out.println("Removing process: " + removeItem);
-            }
-            trickleDownArrayHeap(0);
-            return removeItem;
-        }
-        else{
-            System.out.println("The heapArray is empty");
-            return null;
-        }
-
+        OpCodeClass removedOpCode = (OpCodeClass) heapArray[0];
+        
+        heapArray[0] = heapArray[arraySize - 1];
+        arraySize--;
+        trickleDownArrayHeap(0);
+        return removedOpCode;
     }
-
+    
     /**
-     * Utility method to set the display flag for displaying internal
-     * operations of the heap bubble and trickle operations
+     * Utility method to set the display flag for displaying 
+     * internal operations of the heap bubble and trickle operations
      * @param setState - flag used to set the state to display, or not
      */
     public void setDisplayFlag(boolean setState)
     {
         displayFlag = setState;
     }
-
     /**
      * Dumps array to screen as is, no filtering or management
      */
     public void showArray()
     {
-        if(displayFlag)
+        int index;
+        
+        for(index = 0; index < arraySize; index ++)
         {
-            int index = 0;
-            for(index = 0 ; index < arraySize ; index ++)
-            {
-                System.out.print(heapArray[index].toString() + " ");
-            }
+            System.out.print(heapArray[index].toString() + " - ");
         }
     }
-
+    
     /**
-     * Recursive operation to reset data in the correct order for the min heap after data removal
-     * @param currentIndex - index of current item being assessed, and moved down as required
+     * Recursive operation to reset data in the correct order for the min 
+     * heap after data removal
+     * @param currentIndex - index of current item being 
+     * assessed, and moved down as required
      */
     private void trickleDownArrayHeap(int currentIndex)
     {
-        int largestIndex = currentIndex;
-        int leftChildIndex=0;
-        int rightChildIndex=0;
-        leftChildIndex = (currentIndex + 1) * 2 - 1;
-        rightChildIndex = (currentIndex + 1) * 2;
-        OpCodeClass parentData = heapArray[currentIndex];
-
-        if (leftChildIndex < arraySize && heapArray[leftChildIndex].compareTo(heapArray[largestIndex]) < 0)
+    	int leftIndex = (currentIndex) * 2 + 1;
+    	int rightIndex = (currentIndex) * 2 + 2;
+    	OpCodeClass Op1;
+    	OpCodeClass Op2;
+    	OpCodeClass Op3;
+    	OpCodeClass tempOp;
+    
+    	if( displayFlag )
+    	{
+        System.out.println( "Removing process: " + 
+        		heapArray[ currentIndex ].toString());
+        if( rightIndex < arraySize )
         {
-            largestIndex = leftChildIndex;
+            Op1 = (OpCodeClass) heapArray[leftIndex];
+            Op2 = (OpCodeClass) heapArray[rightIndex];
+            Op3 = (OpCodeClass) heapArray[currentIndex];
+            
+            if(Op1.compareTo(Op3) < 0 || Op2.compareTo(Op3) < 0)
+            {
+                System.out.println(" - Trickle down: ");
+                if(Op1.compareTo(Op2) < 0)
+                {
+                    System.out.println(" -Swapping Parent: " + Op3.toString() + 
+                    		" with left child: " + Op1.toString());
+                    
+                    tempOp = Op3;
+                    heapArray[currentIndex] = heapArray[leftIndex];
+                    heapArray[leftIndex] = tempOp;
+                    trickleDownArrayHeap(leftIndex);
+                }
+                else
+                {
+                    System.out.println(" -Swapping Parent: " + Op3.toString() + 
+                    		" with right child: " + Op2.toString());
+                    
+                    tempOp = Op3; 
+                    heapArray[currentIndex] = heapArray[rightIndex];
+                    heapArray[rightIndex] = tempOp;
+                    trickleDownArrayHeap(rightIndex);
+                }
+            }
+            System.out.println();
         }
-        if (rightChildIndex < arraySize && heapArray[rightChildIndex].compareTo(heapArray[largestIndex]) < 0)
+        else if(leftIndex < arraySize)
         {
-            largestIndex = rightChildIndex;
-        }
-
-        if (largestIndex != currentIndex)
-        {
-            heapArray[currentIndex] = heapArray[largestIndex];
-            heapArray[largestIndex] = parentData;
-            trickleDownArrayHeap(largestIndex);
+            Op1 = (OpCodeClass) heapArray[leftIndex];
+            Op3 = (OpCodeClass) heapArray[currentIndex];
+            
+            if( Op1.compareTo(Op3) < 0)
+            {
+                System.out.println(" -Swapping Parent: " + Op3.toString() + 
+                		" with left child: " + Op1.toString());
+                
+                tempOp = Op3;
+                heapArray[currentIndex] = heapArray[leftIndex];
+                heapArray[leftIndex] = tempOp;
+                trickleDownArrayHeap(leftIndex);
+            }
+            System.out.println();
         }
     }
-
+    else
+    {
+        if(rightIndex < arraySize)
+        {
+            Op1 = (OpCodeClass) heapArray[leftIndex];
+            Op2 = (OpCodeClass) heapArray[rightIndex];
+            Op3 = (OpCodeClass) heapArray[currentIndex];
+            
+            if(Op1.compareTo(Op3) < 0 || Op2.compareTo(Op3) < 0)
+            {
+                if(Op1.compareTo( Op2 ) < 0)
+                {
+                    tempOp = Op3;
+                    heapArray[currentIndex] = heapArray[leftIndex];
+                    heapArray[leftIndex] = tempOp;
+                    trickleDownArrayHeap(leftIndex);
+                }
+                else
+                {
+                    tempOp = Op3; 
+                    heapArray[currentIndex] = heapArray[rightIndex];
+                    heapArray[rightIndex] = tempOp;
+                    trickleDownArrayHeap(rightIndex);
+                }
+            }
+        }
+        else if(leftIndex < arraySize)
+        {
+            Op1 = (OpCodeClass) heapArray[leftIndex];
+            Op3 = (OpCodeClass) heapArray[currentIndex];
+            
+            if(Op1.compareTo(Op3) < 0)
+            {
+                tempOp = Op3;
+                heapArray[currentIndex] = heapArray[leftIndex];
+                heapArray[leftIndex] = tempOp;
+                trickleDownArrayHeap(leftIndex);
+            }
+        }
+    }
+}
 }
